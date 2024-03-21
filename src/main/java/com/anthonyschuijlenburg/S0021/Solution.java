b04ab6d86b99d4c715e737c4e55c561c89b173c4
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-03-21 11:02:10 +0100

    feat: add a solution for S0021

---
+++ src/main/java/com/anthonyschuijlenburg/S0021/Solution.java
@@ -0,0 +1,42 @@
+package com.anthonyschuijlenburg.S0021;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+
+        ArrayList<Integer> foundValues = new ArrayList<>();
+
+        foundValues.addAll(extractValues(list1));
+        foundValues.addAll(extractValues(list2));
+
+        Integer[] foundValuesArray = foundValues.toArray(new Integer[0]);
+
+        Arrays.sort(foundValuesArray, Collections.reverseOrder());
+
+        ListNode newList = new ListNode(foundValuesArray[0]);
+
+        for (int i = 1; i < foundValuesArray.length; i++) {
+            newList = new ListNode(foundValuesArray[i], newList);
+        }
+
+        return newList;
+    }
+
+    public static ArrayList<Integer> extractValues(ListNode list) {
+        ArrayList<Integer> foundValues = new ArrayList<>();
+        ListNode nextNode = list;
+
+        while (nextNode != null) {
+            foundValues.add(nextNode.val);
+            nextNode = nextNode.next;
+        }
+
+        return foundValues;
+    }
+}
---
+++ src/test/java/com/anthonyschuijlenburg/S0021/SolutionTest.java
@@ -0,0 +1,71 @@
+package com.anthonyschuijlenburg.S0021;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+    @Test
+    void mergeTwoLists_simpleValidInput() {
+        Solution solutionGenerator =  new Solution();
+
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode list2 = new ListNode(1, new ListNode(2, new ListNode(3)));
+
+        ListNode result = solutionGenerator.mergeTwoLists(list1, list2);
+
+        assertEquals(
+                new ArrayList<>(Arrays.asList(1, 1, 2, 2, 3, 3)),
+                Solution.extractValues(result)
+        );
+    }
+
+    @Test
+    void mergeTwoLists_complexValidInput() {
+        Solution solutionGenerator =  new Solution();
+
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode list2 = new ListNode(3, new ListNode(6, new ListNode(9)));
+
+        ListNode result = solutionGenerator.mergeTwoLists(list1, list2);
+
+        assertEquals(
+                new ArrayList<>(Arrays.asList(1, 2, 3, 3, 6, 9)),
+                Solution.extractValues(result)
+        );
+    }
+
+    @Test
+    void mergeTwoLists_emptyInput() {
+        Solution solutionGenerator =  new Solution();
+
+        ListNode list1 = null;
+        ListNode list2 = null;
+
+        ListNode result = solutionGenerator.mergeTwoLists(list1, list2);
+
+        assertEquals(
+                new ArrayList<>(),
+                Solution.extractValues(result)
+        );
+    }
+
+    @Test
+    void mergeTwoLists_oneEmptyInput() {
+        Solution solutionGenerator =  new Solution();
+
+        ListNode list1 = null;
+        ListNode list2 = new ListNode();
+
+        ListNode result = solutionGenerator.mergeTwoLists(list1, list2);
+
+        assertEquals(
+                new ArrayList<>(List.of(0)),
+                Solution.extractValues(result)
+        );
+    }
+}
